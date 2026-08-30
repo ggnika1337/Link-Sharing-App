40b17374b2ff38ba50ff185a8571972c58c69071
nodokvi-cmyk <nodo.kvi@gmail.com>
2026-08-30 16:24:40 +0400

    setup

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,15 +1,41 @@
+import dns from 'node:dns';
+
+dns.setServers(['8.8.8.8', '8.8.4.4']);
+
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
 import { MongooseModule } from '@nestjs/mongoose';
+import { LoggerModule } from 'pino-nestjs';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      isGlobal: true
+    }),
+    LoggerModule.forRoot({
+      pinoHttp: {
+        transport: {
+          target: "pino-pretty",
+          options: {singleLine: true}
+        }
+      }
+    }),
+    JwtModule.registerAsync({
+      global: true,
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        secret: configService.get("JWT_SECRET")
+      }),
+      inject: [ConfigService]
+    }),
+    MongooseModule.forRoot(process.env.MONGODB_URI!),
     UsersModule,
     AuthModule,
-    MongooseModule.forRoot(process.env.MONGO_URL as string),
   ],
   controllers: [AppController],
   providers: [AppService],
