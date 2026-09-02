851f40cfdd8d9936e702b8212f9d5a2c1b86e84d
nodokvi-cmyk <nodo.kvi@gmail.com>
2026-09-02 16:44:50 +0400

    auth

--- backend/src/auth/auth.module.ts
+++ backend/src/auth/auth.module.ts
@@ -2,10 +2,15 @@ import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { UsersModule } from '../users/users.module';
+import { MongooseModule } from '@nestjs/mongoose';
+import { UserSchema } from '../users/schemas/user.schema';
 
 @Module({
   imports: [
-    UsersModule
+    UsersModule,
+    MongooseModule.forFeature([
+      {name: "user", schema: UserSchema}
+    ])
   ],
   controllers: [AuthController],
   providers: [AuthService],
